5f612d30bbb630b0f62933580c504b693f8b0bb2
Niran Babalola <niran@niran.org>
2016-05-29 19:53:54 -0500

    Add more transact tests

--- test/transact.js
+++ test/transact.js
@@ -27,8 +27,58 @@ describe('transact', () => {
     expect(sendAsync.calledOnce).to.be.true;
 
     const args = sendAsync.firstCall.args[0];
-    expect(args.params[0]).to.be.deep.equal(tx.options);
-    expect(args.method).to.be.equal('eth_sendTransaction');
+    expect(args.params[0]).to.deep.equal(tx.options);
+    expect(args.method).to.equal('eth_sendTransaction');
+
+    done();
+  });
+
+  it('overrides options', async function (done) {
+    const tx = Transaction({
+      options: {
+        from: '0xe8d3266f3c4f083ab8864b5e04aea7b087044e49',
+        to: '0x7967c4f4512195ba83ae8f08ca30f7b145be6cf8',
+        value: '0xf4240',
+      },
+    });
+
+    const { provider, resultStub } = utils.stubbedProvider();
+    const sendAsync = sinon.spy(provider, 'sendAsync');
+    resultStub.returns(utils.STUB_TXHASH);
+
+    const overrideDestination = '0x533e20fb3df2a62b3fd6c569e8a23701b84dee85';
+    await tx.transact(provider, { to: overrideDestination });
+
+    const args = sendAsync.firstCall.args[0];
+    expect(args.params[0]).not.to.deep.equal(tx.options);
+    expect(args.params[0].to).to.equal(overrideDestination);
+
+    done();
+  });
+
+  it('uses handleTransact when provided', async function (done) {
+    const CUSTOM_HANDLER = 'CUSTOM HANDLER';
+    const handleTransact = sinon.stub();
+    handleTransact.returns(Promise.resolve(CUSTOM_HANDLER));
+
+    const tx = Transaction({
+      options: {
+        from: '0xe8d3266f3c4f083ab8864b5e04aea7b087044e49',
+        to: '0x7967c4f4512195ba83ae8f08ca30f7b145be6cf8',
+        value: '0xf4240',
+      },
+      handleTransact: () => handleTransact(),
+    });
+
+    const { provider } = utils.stubbedProvider();
+    const sendAsync = sinon.spy(provider, 'sendAsync');
+
+    const overrideDestination = '0x533e20fb3df2a62b3fd6c569e8a23701b84dee85';
+    const result = await tx.transact(provider, { to: overrideDestination });
+
+    expect(sendAsync.called).to.be.false;
+    expect(handleTransact.called).to.be.true;
+    expect(result).to.equal(CUSTOM_HANDLER);
 
     done();
   });
